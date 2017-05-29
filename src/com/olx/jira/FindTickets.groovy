7646d874d96390ace78909c310505161865684b5
André Martinho <andre.martinho@olx.com>
2017-05-29 19:25:11 +0100

    Adds the FindTickets Class
    - The FindTickets Class is a class that finds the Jira Tickets
    from a defined branch to current HEAD and then perfoms a transition
    on the specific tickets

---
+++ src/com/olx/jira/FindTickets.groovy
@@ -0,0 +1,31 @@
+package com.olx.jira
+
+class FindTickets implements Serializable{
+
+    def steps
+
+    FindTickets(steps) {
+        this.steps = steps
+    }
+
+    def checkTickets(branch,ticketNomenclature,transitionToMake,gitUrl){
+
+        def tickets = steps.sh(script: "git log ${branch}..HEAD" + //Gets all the commits from a branch to HEAD
+                " | grep ${ticketNomenclature}" + // Gets all the references to a specific nomenclature (ex: GVRECFAND)
+                " | xargs -n 1" + // Removes all the whitespaces
+                " | tr '\n' ','" + // Substitutes the break lines for ","
+                " | sed 's/\\(.*\\),/\\1/'" //Removes the last "," from the String
+                , returnStdout: true)
+
+        if(tickets.size() > 0){
+            steps.jiraTransitionIssues(this,
+                    "https://naspersclassifieds.atlassian.net/rest/api/2/",
+                    "${steps.env.JIRA_AUTH_TOKEN}",
+                    "issue in (${tickets})",
+                    "${transitionToMake}",
+                    "[This was moved as part of PR #${steps.env.CHANGE_ID}|${gitUrl}${steps.env.CHANGE_ID}")
+        }else{
+            steps.echo "No tickets to process. Moving forward"
+        }
+    }
+}
