fcaa6ea9407e5b0fd8b7ad77287713744b5a66a6
André Martinho <andre.martinho@olx.com>
2017-06-05 19:07:28 +0100

    Improves on the Deploy and FindTickets Classes
    - Improves on more options to deploy and more variables to take into
    consideration
    - Improves on FindTickets class by separating the getTickets into a function
    so it can be used by more than only the checkTickets function
    - Adds a checkTicketsInQA to move the tickets in QA and moving the
    logic out of the JenkinsFile

--- src/com/olx/deploy/Deploy.groovy
+++ src/com/olx/deploy/Deploy.groovy
@@ -1,6 +1,6 @@
 package com.olx.deploy
 
-class Deploy implements Serializable{
+class Deploy implements Serializable {
 
     def steps
     def valuesToUse
@@ -9,32 +9,51 @@ class Deploy implements Serializable{
         this.steps = steps
     }
 
-    def setValues(){
-        def distributionGroupsFilePath = ""
+    def setValues() {
         def distributionGroups = ""
         def distributionEmails = ""
 
-        if(steps.env.DISTRIBUTION_GROUPS_FILE_PATH.length() > 0){
-            distributionGroupsFilePath = "-PDISTRIBUTION_GROUPS_FILE_PATH='${steps.env.DISTRIBUTION_GROUPS_FILE_PATH}'"
-        }
-
-        if(steps.env.DISTRIBUTION_GROUPS.length() > 0){
+        if (steps.env.DISTRIBUTION_GROUPS.length() > 0) {
             distributionGroups = "-PDISTRIBUTION_GROUPS='${steps.env.DISTRIBUTION_GROUPS}'"
         }
 
-        if(steps.env.DISTRIBUTION_EMAILS.length() > 0){
+        if (steps.env.DISTRIBUTION_EMAILS.length() > 0) {
             distributionEmails = "-PDISTRIBUTION_EMAILS='${steps.env.DISTRIBUTION_EMAILS}'"
         }
 
-        valuesToUse = "${steps.env.CI_ENVIRONMENT_FILE} -PDISTRIBUTION_NOTES='${steps.env.DISTRIBUTION_NOTES}' ${distributionGroupsFilePath} ${distributionGroups} ${distributionEmails}"
+        valuesToUse = "${steps.env.CI_ENVIRONMENT_FILE} -PDISTRIBUTION_NOTES='${steps.env.DISTRIBUTION_NOTES}' ${distributionGroups} ${distributionEmails}"
     }
 
-    def deployTo(flavour, variant = "Release"){
-        steps.sh "./gradlew ${flavour}${variant}DeliverTask ${valuesToUse}"
+    def deployFlavours(flavoursToIterate, variant = "Release") {
+        for (String flavour : flavoursToIterate){
+            steps.sh "./gradlew ${flavour}${variant}DeliverTask ${valuesToUse}"
+        }
     }
 
-    def deployAll(){
+    def deployUsingWeeklyBuilds(flavoursToIterate, variant = "Release"){
+        if(flavoursToIterate == "All"){
+            flavoursToIterate = steps.sh(script:"for file in app/src/*/; do [[ \$(basename \$file) =~ ^(androidTest|main|test)\$ ]] && continue echo \$(basename \$file) done",returnStdout: true).tokenize(',')
+        }
+
+        for (String flavour : flavoursToIterate){
+            steps.sh "./gradlew ${flavour}${variant}DeliverTask ${getDistributionGroupsFilePath(flavour)} ${valuesToUse}"
+        }
+    }
+
+    def deployAll() {
         steps.sh "./gradlew deliverAllFlavoursTask ${valuesToUse}"
     }
+
+    def getDistributionGroupsFilePath(variant){
+        def distributionGroupsFilePath = ""
+
+        if (steps.env.USE_WEEKLY_DISTRIBUTION_FILE_PATH) {
+            distributionGroupsFilePath = "-PDISTRIBUTION_GROUPS_FILE_PATH=ci_cd/weekly/${variant}/group_aliases.txt"
+        }
+
+        return distributionGroupsFilePath
+    }
+
+
 
 }
--- src/com/olx/jira/FindTickets.groovy
+++ src/com/olx/jira/FindTickets.groovy
@@ -10,12 +10,7 @@ class FindTickets implements Serializable{
 
     def checkTickets(branch,ticketNomenclature,transitionToMake,gitUrl){
 
-        def tickets = steps.sh(script: "git log origin/${branch}..HEAD" + //Gets all the commits from a branch to HEAD
-                " | grep ${ticketNomenclature}" + // Gets all the references to a specific nomenclature (ex: GVRECFAND)
-                " | xargs -n 1" + // Removes all the whitespaces
-                " | tr '\n' ','" + // Substitutes the break lines for ","
-                " | sed 's/\\(.*\\),/\\1/'" //Removes the last "," from the String
-                , returnStdout: true)
+        def tickets = getTickets(branch,ticketNomenclature)
 
         if(tickets.size() > 0){
             steps.jiraTransitionIssues(steps,
@@ -27,5 +22,31 @@ class FindTickets implements Serializable{
         }else{
             steps.echo "No tickets to process. Moving forward"
         }
+    }
+
+    def checkTicketsInQA(branch,ticketNomenclature,transitionToMake){
+
+        def tickets = getTickets(branch,ticketNomenclature)
+
+        if(tickets.size() > 0) {
+            //Transition issues that are on Ready for QA in the Project
+            steps.jiraTransitionIssues(this,
+                    "https://naspersclassifieds.atlassian.net/rest/api/2/",
+                    "${steps.env.JIRA_AUTH_TOKEN}",
+                    "project = GVRECFAND AND status = 'Ready for QA' AND issue in(${tickets})",
+                    "${transitionToMake}",
+                    "This was moved as part of build ${steps.env.BUILD_NUMBER}")
+        }else{
+            steps.echo "No tickets to process. Moving forward"
+        }
+    }
+
+    private getTickets(branch, ticketNomenclature){
+        return steps.sh(script: "git log origin/${branch}..HEAD" + //Gets all the commits from a branch to HEAD
+                " | grep ${ticketNomenclature}" + // Gets all the references to a specific nomenclature (ex: GVRECFAND)
+                " | xargs -n 1" + // Removes all the whitespaces
+                " | tr '\n' ','" + // Substitutes the break lines for ","
+                " | sed 's/\\(.*\\),/\\1/'" //Removes the last "," from the String
+                , returnStdout: true)
     }
 }
