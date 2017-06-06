f0cb97ef82009886157d98a52985a47585acf3fe
André Martinho <andre.martinho@olx.com>
2017-06-06 18:48:14 +0100

    Changes the way that FindTickets handle QA Tickets

--- src/com/olx/jira/FindTickets.groovy
+++ src/com/olx/jira/FindTickets.groovy
@@ -1,6 +1,6 @@
 package com.olx.jira
 
-class FindTickets implements Serializable{
+class FindTickets implements Serializable {
 
     def steps
 
@@ -8,40 +8,33 @@ class FindTickets implements Serializable{
         this.steps = steps
     }
 
-    def checkTickets(branch,ticketNomenclature,transitionToMake,gitUrl){
+    def checkTickets(branch, ticketNomenclature, transitionToMake, gitUrl) {
 
-        def tickets = getTickets(branch,ticketNomenclature)
+        def tickets = getTickets(branch, ticketNomenclature)
 
-        if(tickets.size() > 0){
+        if (tickets.size() > 0) {
             steps.jiraTransitionIssues(steps,
                     "https://naspersclassifieds.atlassian.net/rest/api/2/",
                     "${steps.env.JIRA_AUTH_TOKEN}",
                     "issue in (${tickets})",
                     "${transitionToMake}",
                     "[This was moved as part of PR #${steps.env.CHANGE_ID}|${gitUrl}${steps.env.CHANGE_ID}]")
-        }else{
+        } else {
             steps.echo "No tickets to process. Moving forward"
         }
     }
 
-    def checkTicketsInQA(branch,ticketNomenclature,transitionToMake){
-
-        def tickets = getTickets(branch,ticketNomenclature)
-
-        if(tickets.size() > 0) {
-            //Transition issues that are on Ready for QA in the Project
-            steps.jiraTransitionIssues(this,
-                    "https://naspersclassifieds.atlassian.net/rest/api/2/",
-                    "${steps.env.JIRA_AUTH_TOKEN}",
-                    "project = GVRECFAND AND status = 'Ready for QA' AND issue in(${tickets})",
-                    "${transitionToMake}",
-                    "This was moved as part of build ${steps.env.BUILD_NUMBER}")
-        }else{
-            steps.echo "No tickets to process. Moving forward"
-        }
+    def checkTicketsInQA(ticketNomenclature, transitionToMake) {
+        //Transition issues that are on Ready for QA in the Project
+        steps.jiraTransitionIssues(this,
+                "https://naspersclassifieds.atlassian.net/rest/api/2/",
+                "${steps.env.JIRA_AUTH_TOKEN}",
+                "project = ${ticketNomenclature} AND status = 'Ready for QA'",
+                "${transitionToMake}",
+                "This was moved as part of build ${steps.env.BUILD_NUMBER}")
     }
 
-    private getTickets(branch, ticketNomenclature){
+    private getTickets(branch, ticketNomenclature) {
         return steps.sh(script: "git log origin/${branch}..HEAD" + //Gets all the commits from a branch to HEAD
                 " | grep ${ticketNomenclature}" + // Gets all the references to a specific nomenclature (ex: GVRECFAND)
                 " | xargs -n 1" + // Removes all the whitespaces
