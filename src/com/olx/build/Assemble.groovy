e56ba95f0cb7cefd617faaa529851f92a0321123
André Martinho <andre.martinho@olx.com>
2017-06-14 15:53:45 +0100

    Adds a new method assembleBuildsWithNames
    - Receives the flavours separated by "," and a variant that defaults to "Release"

--- src/com/olx/build/Assemble.groovy
+++ src/com/olx/build/Assemble.groovy
@@ -16,6 +16,15 @@ class Assemble implements Serializable{
         steps.sh "./gradlew ${steps.env.CI_ENVIRONMENT_FILE} -PBUILD_NUMBER=${steps.env.BUILD_NUMBER} ${task}"
     }
 
+    def assembleBuildsWithNames(flavours, variant = "Release"){
+        def flavoursList = flavours.tokenize(',')
+
+        for (String flavour : flavoursList){
+            steps.echo "Assemble ${flavour}${variant}"
+            steps.sh "./gradlew ${steps.env.CI_ENVIRONMENT_FILE} -PBUILD_NUMBER=${steps.env.BUILD_NUMBER} assemble${flavour}${variant}"
+        }
+    }
+
     def archiveResults(){
         steps.archiveFilesFromPath("app/build/outputs/apk/*.apk")
     }
